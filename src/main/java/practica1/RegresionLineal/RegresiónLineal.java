01a7afe8aa7aad3e3df0e7668eda5d872f7409d5
al364113 <al364113@uji.es>
2022-02-22 19:01:46 +0100

    Clase RegresionLineal optimizada 2.

--- src/main/java/practica1/RegresionLineal/RegresiónLineal.java
+++ src/main/java/practica1/RegresionLineal/RegresiónLineal.java
@@ -14,34 +14,34 @@ public class RegresiónLineal {
     }
 
     public void train(Table data) {
-        Double x_media, y_media;
-        double aux_x = 0.0, aux_y = 0.0, sum_numerador = 0, sum_denominador = 0;
+        Double mediaX, mediaY;
+        double sumaX = 0.0, sumaY = 0.0, sumaNumerador = 0, sumaDenominador = 0;
 
         List<Double> x = data.getColumAt(0);
         List<Double> y = data.getColumAt(1);
 
-        for(int i = 0; i < data.size(); i++){
-            aux_x = x.get(i);
-            aux_y = y.get(i);
+        for (int i = 0; i < data.size(); i++) {
+            sumaX = x.get(i);
+            sumaY = y.get(i);
         }
 
         /*List<Double> d;
         for (Row fila : data.getRows()) {
             d = fila.getData();
-            aux_x += d.get(0);
-            aux_y += d.get(1);
+            sumaX += d.get(0);
+            sumaY += d.get(1);
         }*/
-        x_media = aux_x / data.size();
-        y_media = aux_y / data.size();
+        mediaX = sumaX / x.size();
+        mediaY = sumaY / y.size();
 
         /*List<Double> x = data.getColumAt(0);
         List<Double> y = data.getColumAt(1);*/
 
         for (int i = 0; i < data.size(); i++) {
-            sum_numerador += (x.get(i) - x_media) * (y.get(i) - y_media);
-            sum_denominador += Math.pow(x.get(i) - x_media, 2);
+            sumaNumerador += (x.get(i) - mediaX) * (y.get(i) - mediaY);
+            sumaDenominador += Math.pow(x.get(i) - mediaX, 2);
         }
-        alpha = sum_numerador / sum_denominador;
+        alpha = sumaNumerador / sumaDenominador;
     }
 
     public Double estimate(Double sample) {
