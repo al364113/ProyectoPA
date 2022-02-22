d1d72cefa3d78811112496a13a43ea5c7e7efcb0
dIEGOsOLSO <al404832@uji.es>
2022-02-22 23:29:46 +0100

    RegresionLineal acabado

--- src/main/java/practica1/RegresionLineal/RegresionLineal.java
+++ src/main/java/practica1/RegresionLineal/RegresionLineal.java
@@ -6,15 +6,14 @@ import java.lang.Math;
 import java.util.List;
 
 public class RegresionLineal {
-    Double alpha;
-    Double beta;
+    Double alpha, beta;
 
     public RegresionLineal() {
     }
 
     public void train(Table data) {
         Double mediaX, mediaY;
-        double sumaX = 0.0, sumaY = 0.0, sumaNumerador = 0, sumaDenominador = 0;
+        double sumaX = 0.0, sumaY = 0.0, sumaNumerador = 0.0, sumaDenominador = 0.0;
 
         List<Double> x = data.getColumAt(0);
         List<Double> y = data.getColumAt(1);
@@ -23,18 +22,8 @@ public class RegresionLineal {
             sumaX = x.get(i);
             sumaY = y.get(i);
         }
-
-        /*List<Double> d;
-        for (Row fila : data.getRows()) {
-            d = fila.getData();
-            sumaX += d.get(0);
-            sumaY += d.get(1);
-        }*/
-        mediaX = sumaX / x.size();
-        mediaY = sumaY / y.size();
-
-        /*List<Double> x = data.getColumAt(0);
-        List<Double> y = data.getColumAt(1);*/
+        mediaX = sumaX / data.size();
+        mediaY = sumaY / data.size();
 
         for (int i = 0; i < data.size(); i++) {
             sumaNumerador += (x.get(i) - mediaX) * (y.get(i) - mediaY);
