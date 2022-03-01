d59f43b024c190b5e5b71c9beb37df889a0a6d28
dIEGOsOLSO <al404832@uji.es>
2022-03-01 12:49:09 +0100

    metodos knn

--- src/main/java/practica1/KNN/KNN.java
+++ src/main/java/practica1/KNN/KNN.java
@@ -1,4 +1,17 @@
 package practica1.KNN;
 
+import practica1.CSV.TableWithLabels;
+
+import java.util.List;
+
 public class KNN {
+
+    public void train (TableWithLabels data) {
+
+    }
+
+    public String estimate (List<Double> sample) {
+
+
+    }
 }
