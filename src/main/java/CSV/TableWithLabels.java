7610200cf7f65b0383372fff0f82ab3fc443ae29
al364113 <al364113@uji.es>
2022-03-08 13:43:13 +0100

    Creada EmptyTableException

--- src/main/java/CSV/TableWithLabels.java
+++ src/main/java/CSV/TableWithLabels.java
@@ -1,5 +1,7 @@
 package CSV;
 
+import Excepciones.EmptyTableException;
+
 import java.util.List;
 
 public class TableWithLabels extends Table {
@@ -7,7 +9,10 @@ public class TableWithLabels extends Table {
     public TableWithLabels() {
     }
 
-    public TableWithLabels(List<List<String>> tabla, List<String> etiquetas) {
+    public TableWithLabels(List<List<String>> tabla, List<String> etiquetas) throws EmptyTableException {
+        if(tabla == null || tabla.size() == 0){
+            throw new EmptyTableException();
+        }
         headers = tabla.get(0);
         for (int i = 1; i < tabla.size(); i++) {
             filas.add(new RowWithLabel(tabla.get(i), etiquetas.get(i)));
