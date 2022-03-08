24759d1c3428f8b19b7a5b5ba3cd5abf01868506
al364113 <al364113@uji.es>
2022-03-08 14:06:16 +0100

    Creados test de EmptyTableException

---
+++ src/test/java/Excepciones/ExcepcionesTest.java
@@ -0,0 +1,31 @@
+package Excepciones;
+
+import CSV.Row;
+import CSV.Table;
+import CSV.TableWithLabels;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("Test de las excepciones.")
+public class ExcepcionesTest {
+    @Test
+    @DisplayName("Test de la clase EmptyTableException")
+    void EmptyTableExceptionTest() {
+        assertThrows(EmptyTableException.class, () -> new Table(null));
+        List<List<String>> lista = new ArrayList<>();
+        lista.clear();
+        assertThrows(EmptyTableException.class, () -> new Table(lista));
+
+        assertThrows(EmptyTableException.class, () -> new TableWithLabels(null, null));
+
+        assertThrows(EmptyTableException.class, () -> new TableWithLabels(lista, null));
+
+    }
+}
