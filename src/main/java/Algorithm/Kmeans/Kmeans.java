61c7243c1a534c9bb29b4b24189ecb9ce842e696
al364113 <al364113@uji.es>
2022-03-15 12:57:39 +0100

    Creada clase Kmeans, con esqueleto.

---
+++ src/main/java/Algorithm/Kmeans/Kmeans.java
@@ -0,0 +1,24 @@
+package Algorithm.Kmeans;
+
+import Algorithm.Algorithm;
+import CSV.Row;
+import CSV.Table;
+
+public class Kmeans implements Algorithm<Table, String, Row> {
+    public Kmeans() {
+    }
+
+    public Kmeans(int numberClusters, int iteractions, long seed){
+
+    }
+
+    @Override
+    public void train(Table T){
+
+    }
+
+    @Override
+    public String estimate(Row r){
+
+    }
+}
