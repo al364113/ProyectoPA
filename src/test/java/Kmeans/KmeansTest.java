23f884d0555e6ee4aedf75deb2a75b7d913b23bb
al364113 <al364113@uji.es>
2022-03-15 14:26:22 +0100

    Empezados test

---
+++ src/test/java/Kmeans/KmeansTest.java
@@ -0,0 +1,25 @@
+package Kmeans;
+
+import Algorithm.KNN.KNN;
+import Algorithm.Kmeans.Kmeans;
+import Excepciones.DifferentFieldNumberInRawException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import CSV.CSV;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Test de la clase Kmeans")
+public class KmeansTest {
+
+    @Test
+    @DisplayName("Test de la clase Kmeans")
+    void meansTest(){
+        Kmeans kmeans = new Kmeans();
+
+    }
+}
