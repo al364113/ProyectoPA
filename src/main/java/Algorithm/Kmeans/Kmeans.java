dbf616a2e8e8c20c03d6c1b5daf34fad4bf8523e
al364113 <al364113@uji.es>
2022-03-15 13:15:16 +0100

    Kmeans v0.1

--- src/main/java/Algorithm/Kmeans/Kmeans.java
+++ src/main/java/Algorithm/Kmeans/Kmeans.java
@@ -4,16 +4,31 @@ import Algorithm.Algorithm;
 import CSV.Row;
 import CSV.Table;
 
+import java.util.List;
+import java.util.Random;
+
 public class Kmeans implements Algorithm<Table, String, Row> {
+    int numberClusters;
+    int iteractions;
+    long seed;
+
     public Kmeans() {
     }
 
     public Kmeans(int numberClusters, int iteractions, long seed){
-
+        this.numberClusters = numberClusters;
+        this.iteractions = iteractions;
+        this.seed = seed;
     }
 
     @Override
-    public void train(Table T){
+    public void train(Table tabla){
+
+        Random random = new Random(seed);
+
+        for(int i = 0; i<numberClusters; i++){
+            Row r = tabla.getRowAt(random.nextInt());
+        }
 
     }
 
